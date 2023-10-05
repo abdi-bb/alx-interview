2121cbfad984c4424f4e2cdf15120c3ab5ba1c04
Abdi B <abdi201380@gmail.com>
2023-10-05 20:58:06 +0300

    [Function]: Returns bool

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Module: '0-lockboxes'
+'''
+
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    ''''
+    Determines if all the boxes can be opened.
+    '''
+    n = len(boxes)
+    visited = [False] * n
+    stack = [0]  # Start with the first box (box 0)
+
+    while stack:
+        current_box = stack.pop()
+        visited[current_box] = True
+
+        # Check the keys in the current box
+        for key in boxes[current_box]:
+            if not visited[key]:
+                stack.append(key)
+
+    # If all boxes have been visited, return True, otherwise return False
+    return all(visited)
