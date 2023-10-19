6ef350d8de205950edaa1c3f9f50688cb5a74685
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 20:19:54 +0300

    [Script]: Reads stdin and extract some merics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Module: '0-freq_of_status_code'
+Module: '0-status'
 '''
 
 import sys
@@ -8,8 +8,8 @@ import sys
 # intialize variables
 line_count = 0
 total_size = 0
-freq_of_status_code = {'200': 0, '301': 0, '400': 0,
-                       '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
@@ -18,21 +18,21 @@ try:
         try:
             file_size, status_code = int(data[-1]), data[-2]
             total_size += file_size
-            if status_code in freq_of_status_code:
+            if status_code in status:
                 # increase the frequency of the key
-                freq_of_status_code[status_code] += 1
+                status[status_code] += 1
         except BaseException:
             pass
 
         if line_count % 10 == 0:
             print("File size: {:d}".format(total_size))
-            for key, value in sorted(freq_of_status_code.items()):
+            for key, value in sorted(status.items()):
                 if value:
                     print("{}: {}".format(key, value))
 
 except KeyboardInterrupt:
     print("File size: {:d}".format(total_size))
-    for key, value in sorted(freq_of_status_code.items()):
+    for key, value in sorted(status.items()):
         if value:
             print("{}: {}".format(key, value))
     raise
