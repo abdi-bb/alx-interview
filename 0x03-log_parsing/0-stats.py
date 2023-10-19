6209f4a1c0b134f0bbce21298d2ac4505f91e526
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 20:07:45 +0300

    [Script]: Reads stdin and extract some merics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,15 +8,15 @@ import sys
 # intialize variables
 line_count = 0
 total_size = 0
-freq_of_status_code = {200: 0, 301: 0, 400: 0,
-                       401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+freq_of_status_code = {'200': 0, '301': 0, '400': 0,
+                       '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
         line_count += 1
         data = line.split()
         try:
-            file_size, status_code = int(data[-1]), int(data[-2])
+            file_size, status_code = int(data[-1]), data[-2]
             total_size += file_size
             if status_code in freq_of_status_code:
                 # increase the frequency of the key
