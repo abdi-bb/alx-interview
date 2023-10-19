dcc27ae4b055d053a14dfb2fc8c9f9fb7f6db354
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 20:59:32 +0300

    [Script]: Reads stdin and extract some merics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,8 +8,8 @@ import sys
 # intialize variables
 line_count = 0
 total_size = 0
-freq_of_status_code = {'200': 0, '301': 0, '400': 0,
-                       '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+stat_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+             '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
@@ -18,27 +18,38 @@ try:
         try:
             file_size, status_code = int(data[-1]), data[-2]
             total_size += file_size
-            if status_code in freq_of_status_code:
+            if status_code in stat_dict:
                 # increase the frequency of the key
-                freq_of_status_code[status_code] += 1
+                stat_dict[status_code] += 1
         except BaseException:
             pass
         # print after counting 10 lines
         if line_count % 10 == 0:
+            # print_metrics(stat_dict, total_size)
             print(f'File size: {total_size:d}')
-            for key, value in sorted(freq_of_status_code.items()):
+            for key, value in sorted(stat_dict.items()):
                 if value > 0:
                     print(f'{key}: {value}')
     # print after finishing the lines from stdin
+    # print_metrics(stat_dict, total_size)
     print(f'File size: {total_size:d}')
-    for key, value in sorted(freq_of_status_code.items()):
+    for key, value in sorted(stat_dict.items()):
         if value > 0:
             print(f'{key}: {value}')
 
 except KeyboardInterrupt:
     # print if 'ctrl + c'
+    # print_metrics(stat_dict, total_size)
     print(f'File size: {total_size:d}')
-    for key, value in sorted(freq_of_status_code.items()):
+    for key, value in sorted(stat_dict.items()):
         if value > 0:
             print(f'{key}: {value}')
     raise
+
+'''
+def print_metrics(stat_dict, total_size):
+    print(f'File size: {total_size:d}')
+    for key, value in sorted(stat_dict.items()):
+        if value > 0:
+            print(f'{key}: {value}')
+'''
