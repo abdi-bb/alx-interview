4165c5a30830f2aec0f757398db58bdecb2eab30
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 17:54:00 +0300

    [Update]: Use split instead of regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,36 +4,32 @@ Module: '0-stats'
 '''
 
 import sys
-import re
+
 
 # intialize variables
-count_line = 0
+line_count = 0
 total_size = 0
 freq_of_status_code = {200: 0, 301: 0, 400: 0,
                        401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-# regex for the line format
-line_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] ' \
-              r'"GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-
 
 try:
     for line in sys.stdin:
-        match = re.match(line_pattern, line)
-        if match:
-            ip_address, date, status_code, file_size = match.groups()
-            count_line += 1
-            total_size += int(file_size)
-            if int(status_code) in freq_of_status_code:
-                # increase the frequency of the key
-                freq_of_status_code[int(status_code)] += 1
+        data = line.split()
+        if len(data) >= 3:
+            status_code, file_size = data[-2], data[-1]
+        line_count += 1
+        total_size += int(file_size)
+        if int(status_code) in freq_of_status_code:
+            # increase the frequency of the key
+            freq_of_status_code[int(status_code)] += 1
 
-        if count_line == 10:
+        if line_count == 10:
             print(f'File size: {total_size}')
             for key in sorted(freq_of_status_code.keys()):
                 if freq_of_status_code[key] > 0:
                     print(f'{key}: {int(freq_of_status_code[key])}')
             # reset the variables back to their intial value
-            count_line = 0
+            line_count = 0
             total_size = 0
             freq_of_status_code = {200: 0, 301: 0, 400: 0,
                                    401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
