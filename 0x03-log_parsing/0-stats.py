a1cdba1f8058fd7b53ded54d759717242f31919b
Abdi Berhe <abdiberhe@gmail.com>
2023-10-19 19:07:23 +0300

    [Update]: Use split instead of regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,10 +6,6 @@ Module: '0-stats'
 import sys
 import re
 
-# regex for the line format
-LINE_PATTERN = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] ' \
-    r'"GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-
 # intialize variables
 line_count = 0
 total_size = 0
@@ -18,17 +14,19 @@ freq_of_status_code = {200: 0, 301: 0, 400: 0,
 
 try:
     for line in sys.stdin:
-        match = re.match(LINE_PATTERN, line)
-        if match:
-            ip_address, date, status_code, file_size = match.groups()
-            line_count += 1
+        line_count += 1
+        data = line.split()
+        try:
+            status_code, file_size = int(data[-2]), int(data[-1])
             total_size += int(file_size)
             if int(status_code) in freq_of_status_code:
                 # increase the frequency of the key
                 freq_of_status_code[int(status_code)] += 1
+        except BaseException:
+            pass
 
         if line_count == 10:
-            print(f'File size: {total_size}')
+            print(f'File size: {total_size:d}')
             for key in sorted(freq_of_status_code.keys()):
                 if freq_of_status_code[key] > 0:
                     print(f'{key}: {int(freq_of_status_code[key])}')
@@ -38,7 +36,7 @@ try:
             freq_of_status_code = {200: 0, 301: 0, 400: 0,
                                    401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 except KeyboardInterrupt:
-    print(f'File size: {total_size}')
+    print(f'File size: {total_size:d}')
     for key in sorted(freq_of_status_code.keys()):
         if freq_of_status_code[key] > 0:
             print(f'{key}: {int(freq_of_status_code[key])}')
