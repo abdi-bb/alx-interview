5e70f34c02eee49179813541458a850949f6bb2c
Abdi B <abdiberhe@gmail.com>
2023-10-24 17:30:57 +0300

    [Update]: Use bitwise & operator instead of bin() method

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,20 +12,19 @@ def validUTF8(data):
     bytes_tobe_checked = 0
 
     for num in data:
-        bin_num = format(num, '08b')
         if bytes_tobe_checked == 0:
-            if bin_num[:1] == '0':
+            if num & 128 == 0:
                 bytes_tobe_checked = 0
-            elif bin_num[:3] == '110':
+            elif num & 224 == 192:
                 bytes_tobe_checked = 1
-            elif bin_num[:4] == '1110':
+            elif num & 240 == 224:
                 bytes_tobe_checked = 2
-            elif bin_num[:5] == '11110':
+            elif num & 248 == 240:
                 bytes_tobe_checked = 3
             else:
                 return False
         else:
-            if bin_num[:2] != '10':
+            if num & 192 != 128:
                 return False
             bytes_tobe_checked -= 1
     if bytes_tobe_checked == 0:
