40f2e5cfe263cfde77b6e25efa4d279f2dfea7e8
Abdi Berhe <abdiberhe@gmail.com>
2023-11-09 11:09:56 -0800

    [Update]: Use request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,54 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
 // Get Movie ID from Command Line
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.log('Usage: node script.js <Movie_ID>');
+  console.log('Usage: ./0-starwars_characters.js <Movie_ID>');
   process.exit(1);
 }
 
 // Make API Request
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-axios.get(url)
-  .then(response => {
-    if (response.status !== 200) {
-      console.error(`Error: Unable to fetch data. Status Code: ${response.status}`);
-      process.exit(1);
-    }
-
-    // Extract Characters
-    const characters = response.data.characters;
-
-    // Display Characters Sequentially
-    displayCharactersSequentially(characters);
-  })
-  .catch(error => {
-    console.error(`Error: ${error.message}`);
+request(url, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error(`Error: Unable to fetch data. Status Code: ${response ? response.statusCode : 'Unknown'}`);
     process.exit(1);
-  });
+  }
+
+  // Extract Characters
+  const characters = JSON.parse(body).characters;
+
+  // Display Characters Sequentially
+  displayCharactersSequentially(characters);
+});
 
 // Display Characters Sequentially
-async function displayCharactersSequentially (characters) {
-  for (const characterUrl of characters) {
-    try {
-      const characterResponse = await axios.get(characterUrl);
-      console.log(characterResponse.data.name);
-    } catch (error) {
-      console.error(`Error fetching character: ${error.message}`);
+function displayCharactersSequentially (characters) {
+  const index = 0;
+
+  function fetchCharacter (index) {
+    if (index >= characters.length) {
+      // All characters fetched, exit the process
+      process.exit(0);
     }
+
+    const characterUrl = characters[index];
+    request(characterUrl, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error(`Error fetching character: ${error ? error.message : 'Unknown'}`);
+      } else {
+        console.log(JSON.parse(body).name);
+      }
+
+      // Fetch the next character
+      fetchCharacter(index + 1);
+    });
   }
+
+  // Start fetching characters
+  fetchCharacter(index);
 }
