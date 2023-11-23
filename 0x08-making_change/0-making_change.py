bae6b704d90fc5886183b02f9271871a66d98896
Abdi Berhe <abdiberhe@gmail.com>
2023-11-23 22:19:02 +0300

    [Update]: Update efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,19 +5,20 @@ Module: '0-making_change'
 
 
 def makeChange(coins, total):
+    '''Returns numbers of coin to meet yhe given total'''
     if total <= 0:
         return 0
 
-    # Sort coins in descending order
     coins.sort(reverse=True)
-
-    # Initialize an array to store
-    # the minimum number of coins needed for each value from 0 to total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Zero coins needed to make change for total = 0
-
-    for coin in coins:
-        for value in range(coin, total + 1):
-            dp[value] = min(dp[value], dp[value - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    sum = 0
+    i = 0
+    counter = 0
+    num_coins = len(coins)
+    while sum < total and i < num_coins:
+        while coins[i] <= total - sum:
+            sum += coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+        i += 1
+    return -1
