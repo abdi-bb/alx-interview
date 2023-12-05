fb94d0d7c4796ff075eb4926ca6c194ffcf08d44
Abdi Berhe <abdiberhe@gmail.com>
2023-12-05 15:21:25 +0300

    fix: Consider arg 'x'

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -18,24 +18,25 @@ def isWinner(x, nums):
     '''main func'''
     maria = 0
     ben = 0
-    for n in nums:
-        numbers = list(range(1, n + 1))
-        maria_turn = True
-        while numbers:
-            can_move = False
-            for i in reversed(numbers):
-                if is_prime(i):
-                    # Remove the number and its multiples from the list
-                    numbers = [j for j in numbers if j % i != 0]
-                    can_move = True
+    for _ in range(x):
+        for n in nums:
+            numbers = list(range(1, n + 1))
+            maria_turn = True
+            while numbers:
+                can_move = False
+                for i in reversed(numbers):
+                    if is_prime(i):
+                        # Remove the number and its multiples from the list
+                        numbers = [j for j in numbers if j % i != 0]
+                        can_move = True
+                        break
+                if not can_move:
+                    if maria_turn:
+                        ben += 1
+                    else:
+                        maria += 1
                     break
-            if not can_move:
-                if maria_turn:
-                    ben += 1
-                else:
-                    maria += 1
-                break
-            maria_turn = not maria_turn
+                maria_turn = not maria_turn
     if maria > ben:
         return "Maria"
     elif ben > maria:
