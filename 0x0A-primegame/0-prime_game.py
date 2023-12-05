e30655100e0dd9c781e70394a55bcf94585bb559
Abdi Berhe <abdiberhe@gmail.com>
2023-12-05 15:44:43 +0300

    fix: Consider arg x and cleanup

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,7 +4,7 @@ Module: '0-prime_game'
 '''
 
 
-def is_prime(n):
+def isPrime(n):
     '''helper func'''
     if n < 2:
         return False
@@ -16,30 +16,11 @@ def is_prime(n):
 
 def isWinner(x, nums):
     '''main func'''
-    maria = 0
-    ben = 0
-    for _ in range(x):
-        for n in nums:
-            numbers = list(range(1, n + 1))
-            maria_turn = True
-            while numbers:
-                can_move = False
-                for i in reversed(numbers):
-                    if is_prime(i):
-                        # Remove the number and its multiples from the list
-                        numbers = [j for j in numbers if j % i != 0]
-                        can_move = True
-                        break
-                if not can_move:
-                    if maria_turn:
-                        ben += 1
-                    else:
-                        maria += 1
-                    break
-                maria_turn = not maria_turn
-    if maria > ben:
-        return "Maria"
-    elif ben > maria:
-        return "Ben"
-    else:
+    maria_win, ben_win = 0, 0
+    for _, n in zip(range(x), nums):
+        primes_count = sum(1 for i in range(1, n + 1) if isPrime(i))
+        ben_win += primes_count % 2 == 0
+        maria_win += primes_count % 2 == 1
+    if maria_win == ben_win:
         return None
+    return 'Maria' if maria_win > ben_win else 'Ben'
